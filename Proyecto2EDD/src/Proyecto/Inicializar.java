56b5a89a9e86119acb02744bde7386736d7a143f
Eacosta <esteacosta@Estebans-MacBook-Pro.local>
2024-11-19 21:09:08 -0400

    Continuacion de recorrido del Json. Agregué el recorrido de los datos de las personas

--- Proyecto2EDD/src/Proyecto/Inicializar.java
+++ Proyecto2EDD/src/Proyecto/Inicializar.java
@@ -60,29 +60,87 @@ public class Inicializar {
                 
                 for (String nombre : personas){
                     
+                    Nodo Nperson;
+                    
                     // Obtiene la lista json de datos de la persona y la
                     // convierte a un array
                     JsonElement Json_datos = Datos.get(nombre);
                     JsonArray l_datos = Json_datos.getAsJsonArray();
                     
-                    //Aquí debe buscarse si el nodo existe, y en
-                    //caso contrario crearlo
+                    //Se declaran los String de los datos de la persona
+                    String ohn = null;
+                    String bt = null;
+                    String kta = null;
+                    String ht = null;
+                    String wt = null;
+                    String oe = null;
+                    String of = null;
+                    String Notas = null;
+                    String Destino = null;
+                    
+                    Array hijos;
                     
                     for (int j = 0; j < l_datos.size(); j++){
                         
                         JsonObject Propiedades = l_datos.get(j).getAsJsonObject();
                         Set<String> properties = Propiedades.keySet();
                         
+                        
                         for (String dato : properties){
                             
-                            if (dato.equals("Text Here")){
+                            //Se obtienen los datos del JsonObject y se
+                            //establecen a las variables declaradas
+                            if (dato.equals("Of his name")){
+                                ohn = Propiedades.get(dato).getAsString();
+                                
+                            } else if (dato.equals("Born to")){
+                                bt = Propiedades.get(dato).getAsString();
+                                
+                            } else if (dato.equals("Known throughout as")){
+                                kta = Propiedades.get(dato).getAsString();
+                                
+                            } else if (dato.equals("Held title")){
+                                ht = Propiedades.get(dato).getAsString();
+                                
+                            } else if (dato.equals("Wed to")){
+                                wt = Propiedades.get(dato).getAsString();
+                                
+                            } else if (dato.equals("Of eyes")){
+                                oe = Propiedades.get(dato).getAsString();
+                                
+                            } else if (dato.equals("Of hair")){
+                                of = Propiedades.get(dato).getAsString();
+                                
+                            } else if (dato.equals("Notes")){
+                                Notas = Propiedades.get(dato).getAsString();
+                                
+                            } else if (dato.equals("Fate")){
+                                Destino = Propiedades.get(dato).getAsString();
+                                
+                            } else if (dato.equals("Father to")){
+                                
+                                //Se obtiene la lista de hijos como JsonArray
+                                JsonElement Json_hijos = Propiedades.get(dato);
+                                JsonArray l_hijos = Json_hijos.getAsJsonArray();
+                                
+                                //Se itera la lista y se pasan los nombres a
+                                // un array de Strings
                                 
                             }
                             
+                            
                         }
                         
+                       
                     }
                     
+                    if (this.arbol.esVacio()){
+                        Persona person = new Persona(nombre, ohn, bt, kta, ht, wt, oe, of, Notas, Destino);
+                        Nperson = new Nodo(person);
+                        this.arbol.setpRoot(Nperson);
+                    } else {
+                        
+                    }
                 }
             }
             
